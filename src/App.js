e4e21ff925694bb9f7da891635f68a9cd8ab61da
Yienisha Abeyratne <yienishaedu@gmail.com>
2025-03-24 01:27:37 -0400

    Added home page and navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Container,
+    Card,
+    CardContent,
+    ListItem,
+    List, ListItemText
+} from '@mui/material';
+import logo from './images/logo.webp';
+import NavTabs from "./components/navigation";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [vehicles, setVehicles] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/vehicles')
+            .then(response => setVehicles(response.data))
+            .catch(error => console.error('Error fetching vehicles:', error));
+    }, []);
+
+
+    return (
+        <div className="bg-[url('./images/background.jpg')] bg-cover bg-center min-h-screen">
+
+            <Toolbar>
+                <img src={logo} alt="Logo" className="w-32 h-1/2 mx-1.5 mt-4"/>
+                <Typography variant="h6">Green Charge Auto</Typography>
+                <NavTabs/>
+            </Toolbar>
+
+            <Container sx={{marginTop: 4}}>
+                <Card sx={{maxWidth: 400, marginTop: 2}}>
+                    <CardContent>
+                        {/*<List>*/}
+                        {/*    {vehicles.map(v => (*/}
+                        {/*        <ListItem key={v.id}>*/}
+                        {/*            <ListItemText primary={`${v.brand} ${v.name} ${v.model}`} />*/}
+                        {/*        </ListItem>*/}
+                        {/*    ))}*/}
+                        {/*</List>*/}
+                    </CardContent>
+                </Card>
+            </Container>
+        </div>
+    );
 }
 
 export default App;

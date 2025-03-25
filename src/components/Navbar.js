03627532bf82c67f15b87f77d95b1ae70f138ce1
Kyle Truong <kyle.trng@gmail.com>
2025-03-25 11:31:56 -0400

    Redesign navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,91 +1,74 @@
-import { Link } from 'react-router-dom';
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import { Link, useLocation } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  Container,
+  Box,
+} from "@mui/material";
+import logo from "../images/logo.webp";
+import { useState } from "react";
 
-function samePageLinkNavigation(event) {
-    if (
-        event.defaultPrevented ||
-        event.button !== 0 || // ignore everything but left-click
-        event.metaKey ||
-        event.ctrlKey ||
-        event.altKey ||
-        event.shiftKey
-    ) {
-        return false;
-    }
-    return true;
+const navLinks = [
+  { label: "Home", link: "/" },
+  { label: "Vehicles", link: "/vehicles" },
+  { label: "Deals", link: "/hotdeals" },
+  { label: "Loan Calculator", link: "/loan" },
+];
+
+function Navbar() {
+  const path = useLocation().pathname;
+  const [menu, showMenu] = useState(false);
+
+  return (
+    <AppBar position="static" sx={{ bgcolor: "#4ade80" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters sx={{ height: "100px" }}>
+          <img
+            className="mr-5"
+            height={100}
+            width={100}
+            src={logo}
+            alt="Logo"
+          />
+          <Box
+            sx={{ flexGrow: 1, display: "flex", justifyContent: "flex-end" }}
+          >
+            {navLinks.map((link, index) => (
+              <Button
+                key={index}
+                component={Link}
+                to={link.link}
+                sx={{
+                  color: path === link.link ? "blue" : "white",
+                  textTransform: "uppercase",
+                  height: "100%",
+                  "&:hover": {
+                    backgroundColor:
+                      path === link.link
+                        ? "rgba(0, 255, 0, 0.1)"
+                        : "rgba(255, 255, 255, 0.1)",
+                  },
+                  marginX: "20px",
+                }}
+              >
+                <Typography variant="h6">{link.label}</Typography>
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ display: "flex" }}>
+            <Button component={Link} to="/cart" sx={{ marginRight: "20px" }}>
+              <img height={75} width={75} src={logo} alt="Cart" />
+            </Button>
+            <Button onClick={() => showMenu(!menu)}>
+              <img height={100} width={100} src={logo} alt="Menu" />
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
-
-function LinkTab(props) {
-    return (
-        <Tab
-            component="a"
-            aria-current={props.selected && 'page'}
-            {...props}
-        />
-    );
-}
-
-LinkTab.propTypes = {
-    selected: PropTypes.bool,
-};
-
-
-const Navbar = () => {
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        // event.type can be equal to focus with selectionFollowsFocus.
-        if (
-            event.type !== 'click' ||
-            (event.type === 'click' && samePageLinkNavigation(event))
-        ) {
-            setValue(newValue);
-        }
-    };
-    return (
-        <Box sx={{ width: '100%' }}>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                aria-label="nav tabs example"
-                role="navigation"
-            >
-                <LinkTab label="Home" href="/"/>
-                <LinkTab label="Vehicles" href="/vehicles"/>
-                <LinkTab label="Deals" href="/trash"/>
-                <LinkTab label="Loan Calculator" href="/loan"/>
-
-            </Tabs>
-        </Box>
-    );
-
-
-};
 
 export default Navbar;
-
-
-import { Link } from '@/components/link'
-import { Navbar, NavbarItem, NavbarSection } from '@/components/navbar'
-import { Logo } from './logo'
-
-function Example() {
-    return (
-        <Navbar>
-            <Link href="/" aria-label="Home">
-                <Logo className="size-10 sm:size-8" />
-            </Link>
-            <NavbarSection>
-                <NavbarItem href="/" current>
-                    Home
-                </NavbarItem>
-                <NavbarItem href="/events">Events</NavbarItem>
-                <NavbarItem href="/orders">Orders</NavbarItem>
-            </NavbarSection>
-        </Navbar>
-    )
-}
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import logo from "../images/logo.webp";
+
 
 const MainLayout = ({ children }) => {
     return (
         <>
-            <img className="h-24 w-auto" src={logo} />
+            
             <Navbar />
             <main className="p-4">{children}</main>
         </>

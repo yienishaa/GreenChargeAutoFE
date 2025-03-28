3200aded14f97167e91973bb67bfcb57231ce5b9
Yienisha Abeyratne <yienishaedu@gmail.com>
2025-03-28 02:03:50 -0400

    Moving Vehicles admin code to new file

--- src/pages/LoanCalculator.js
+++ src/pages/LoanCalculator.js
@@ -1,5 +1,21 @@
+import {Label} from "@mui/icons-material";
+import {FormControl, TextField, Box, Typography} from "@mui/material";
+import * as React from "react";
+
 const LoanCalculator = () => {
-    return <h1 className="text-2xl font-bold">Welcome to NotFound Page</h1>;
+    return(
+        <>
+            <Box className="flex justify-center pt-24 px-4">
+                <Typography ml={15} mr={15} variant={"h6"}>Loan Calculator</Typography>
+            </Box>
+            <Box>
+                <FormControl defaultValue="" required>
+                    <Label>Name</Label>
+                    <TextField placeholder="Write your name here" />
+                </FormControl>
+            </Box>
+        </>
+    );
 };
 
 export default LoanCalculator;

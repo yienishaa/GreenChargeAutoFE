ef54b6b90bb55f409bb381e19ea46b20aa6a0ddc
Yienisha Abeyratne <yienishaedu@gmail.com>
2025-03-28 00:16:18 -0400

    Upload image added

--- src/pages/Vehicles.js
+++ src/pages/Vehicles.js
@@ -9,11 +9,11 @@ const Vehicles = () => {
   const [vehicles, setVehicles] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-/*
+
   useEffect(() => {
     const fetchVehicles = async () => {
       try {
-        const response = await axios.get("http://localhost:8080/vehicles");
+        const response = await axios.get("http://localhost:8080/vehicles/admin/all");
         setVehicles(response.data); // Assuming the response data is an array of vehicles
       } catch (e) {
         setError(e.message);
@@ -32,12 +32,18 @@ const Vehicles = () => {
   if (error) {
     return <div>Error: {error}</div>;
   }
-*/
+
   return (
     <>
       <Grid container spacing={2}>
         <Grid item>
-          <img src={logo}/>
+          {vehicles.map((v) => (
+              <li key={v.id}>
+                <strong>{v.brand} {v.model}</strong><br/>
+                <img src={`https://greencharge-catalog.s3.us-east-1.amazonaws.com/${v.image}`}  />
+              </li>
+          ))}
+
         </Grid>
       </Grid>
     </>
